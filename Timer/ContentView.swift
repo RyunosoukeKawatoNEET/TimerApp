06ed60b2d12c33efb0f5c5d0fb25b196fa67402b
RyunosukeKawatoNEET <kawa108823@icloud.com>
2022-07-31 16:08:33 +0900

    Initial Commit

---
+++ Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Timer
+//
+//  Created by 川戸竜之介 on 2022/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 川戸竜之介 on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
