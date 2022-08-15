b892c6f638958285141efccf12e2b90e0f8afc90
KawatoRyunosuyke <kawa108823@icloud.com>
2022-08-15 14:17:12 +0900

    FirstCommit

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var hour: Int = 8
+    @State private var minute: Int = 30
+    @State private var seconds: Int = 30
+    
+    @State var page : Int? = 0
+    @State var isShowSecondView = false
+    
+    @State var screen: CGSize!
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack() {
+                HStack(spacing : 0){
+                    TimePicker(num: $hour, maxnum : 24).frame(width: 70)
+                    Text("時間").frame(width: 50, height: 50, alignment: .leading)
+                    TimePicker(num: $minute, maxnum : 60).frame(width: 70)
+                    Text("分").frame(width:32,height: 50, alignment: .leading)
+                    TimePicker(num: $seconds, maxnum : 60).frame(width: 70)
+                    Text("秒").frame(width:32,height: 50, alignment: .leading)
+                }
+                
+                Button(action: {
+                    page = 1
+                }) {
+                    Text("モーダル遷移")
+                        .padding()
+                }.frame(width: 200.0, height: 60.0).foregroundColor(.white).background(gradientView(start:Color.orange,end: Color.orange.opacity(0.5))).cornerRadius(20)
+                
+                NavigationLink(destination : SecondView(hour: self.$hour, minute:self.$minute , seconds: self.$seconds),tag: 1, selection: $page) {  }
+            }
+        }
+    }
+}
+
+
+
+struct SecondView: View {
+    @Binding var hour: Int
+    @Binding var minute: Int
+    @Binding var seconds: Int
+
+    var body: some View {
+        Text("\(hour)")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portraitUpsideDown)
     }
+}
+
+func gradientView(start: Color, end: Color) -> LinearGradient {
+    return LinearGradient(
+        gradient: Gradient(colors: [start, end]),
+        startPoint: .leading,
+        endPoint: .trailing)
 }
---
+++ Timer/TimePicker.swift
@@ -0,0 +1,69 @@
+//
+//  TimePicker.swift
+//  Timer
+//
+//  Created by 川戸竜之介 on 2022/07/31.
+//
+import SwiftUI
+
+struct TimePicker: UIViewRepresentable {
+    
+    var num: Binding<Int>
+    var maxnum: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+    func makeUIView(context: UIViewRepresentableContext<TimePicker>) -> UIPickerView {
+        let picker = UIPickerView()
+        
+        picker.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        
+        picker.dataSource = context.coordinator
+        picker.delegate = context.coordinator
+        
+        return picker
+    }
+    
+    func updateUIView(_ view: UIPickerView, context: UIViewRepresentableContext<TimePicker>) {
+        view.selectRow(num.wrappedValue, inComponent: 0, animated: false)
+    }
+    
+    class Coordinator: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
+        var parent: TimePicker
+        
+        init(_ pickerView: TimePicker) {
+            parent = pickerView
+        }
+        ///列数の定義
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        ///各行の値の最大値を定義
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return parent.maxnum
+        }
+        
+        ///Pickerの描画の幅
+        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+            return 60
+        }
+        
+        ///Pickerの描画の高さ
+        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 45
+        }
+        
+        ///フォーマットの指定とComponentの返り値の数値をString型にして出力
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return String(format: "%2d", row)
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+                parent.num.wrappedValue = row
+        }
+    }
+}
+
