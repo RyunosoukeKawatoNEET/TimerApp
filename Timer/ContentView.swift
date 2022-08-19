88753d6348ddfc3d126a0f4f47d9fdf7dd278e56
KawatoRyunosuyke <kawa108823@icloud.com>
2022-08-19 18:02:30 +0900

    セット数、休憩の画面を実装

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -12,6 +12,11 @@ struct ContentView: View {
     @State private var minute: Int = 30
     @State private var seconds: Int = 30
     
+    //オプション設定
+    @State  var setnum: Int = 1
+    @State  var showPicker: Bool = false
+    
+    //画面遷移
     @State var page : Int? = 0
     @State var isShowSecondView = false
     
@@ -28,17 +33,52 @@ struct ContentView: View {
                     TimePicker(num: $seconds, maxnum : 60, minnum :5).frame(width: 70)
                     Text("秒").frame(width:32,height: 50, alignment: .leading)
                 }
-                HStack(spacing:0){
-                    //TimePicker()
+                HStack(spacing:20){
+                    //オプションの選択を可能にする
+                    Button(action: {
+                        withAnimation(.default){
+                            //Pickerの表示・非表示を切り替える
+                            self.showPicker.toggle()
+                        }
+                    }) {
+                        VStack(){
+                            Text("セット数").padding(.horizontal).padding(.top)
+                            Text("\(self.setnum) 回").padding()
+                            Spacer()
+                        }
+                        .frame(width: 150.0, height: 80.0)
+                        .foregroundColor(.white).background(Color.blue).cornerRadius(10).padding(.leading)
+                    }
+                    //オプションの選択を可能にする
+                    Button(action: {
+                        withAnimation(.default){
+                            //Pickerの表示・非表示を切り替える
+                            self.showPicker.toggle()
+                        }
+                    }) {
+                        VStack(){
+                            Text("休憩").padding(.horizontal).padding(.top)
+                            Text("\(setnum)" + "秒").padding()
+                            Spacer()
+                        }
+                        .frame(width: 150.0, height: 80.0)
+                        .foregroundColor(.white)
+                        .background(Color.green).cornerRadius(10).padding(.trailing)
+                    }
+                    
                 }
-                Button(action: {
-                    page = 1
-                }) {
-                    Text("開始")
-                        .padding()
-                }.frame(width: 200.0, height: 60.0).foregroundColor(.white).background(gradientView(start:Color.orange,end: Color.orange.opacity(0.5))).cornerRadius(20)
-                
-                NavigationLink(destination : SecondView(totaltime:CGFloat((self.hour*3600)+(self.minute*60)+self.seconds)),tag: 1, selection: $page) {  }
+                ZStack{
+                    Button(action: {
+                        page = 1
+                    }) {
+                        Text("開始")
+                            .padding()
+                    }.frame(width: 320.0, height: 60.0).foregroundColor(.white).background(gradientView(start:Color.orange,end: Color.orange.opacity(0.5))).cornerRadius(10).padding(.horizontal)
+                    
+                    NavigationLink(destination : SecondView(totaltime:CGFloat((self.hour*3600)+(self.minute*60)+self.seconds)),tag: 1, selection: $page) {  }
+                    
+                    playerPicker(setnum: self.$setnum, showPicker: self.$showPicker).offset(y: self.showPicker ? 0 : UIScreen.main.bounds.height).frame(alignment: .bottom)
+                }
             }
         }
     }
@@ -50,6 +90,35 @@ struct ContentView_Previews: PreviewProvider {
             .previewInterfaceOrientation(.portrait)
     }
 }
+
+struct playerPicker: View {
+    @Binding  var setnum: Int
+    @Binding  var showPicker: Bool
+    
+    var body: some View {
+        VStack(){
+            Button(action: {
+                withAnimation(.default){
+                    self.showPicker = false
+                }
+            }) {
+                HStack {
+                    Spacer() //右寄せにするため使用
+                    Text("完了")
+                        .padding(.horizontal, 15.0)
+                        .padding(.vertical, 5.0)
+                }
+            }
+            //ユーザを入力するピッカー
+            Picker(selection: self.$setnum, label: Text("")) {
+                ForEach(1..<51){index in
+                    Text("\(index)")
+                }
+            }.pickerStyle(WheelPickerStyle()).background(Color(red: 0.95, green: 0.95, blue: 0.97, opacity: 1.0))
+        }.background(Color(red: 0.9, green: 0.9, blue: 0.92, opacity: 1.0)).frame(height:60)
+    }
+}
+
 
 func gradientView(start: Color, end: Color) -> LinearGradient {
     return LinearGradient(
