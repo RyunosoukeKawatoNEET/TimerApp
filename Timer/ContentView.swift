33f0a9e6d6a5b149512dcb9a7f813b1eb15bfd3c
KawatoRyunosuyke <kawa108823@icloud.com>
2022-08-26 13:43:00 +0900

    タイマー完成

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -9,28 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var hour: Int = 0
-    @State private var minute: Int = 30
-    @State private var seconds: Int = 30
+    @State private var minute: Int = 0
+    @State private var seconds: Int = 0
     
     //オプション設定
     @State  var setNum: Int = 1
     @State  var restSecond: Int = 5
     @State  var showPickerSet: Bool = false
     @State  var showPickerRest: Bool = false
-        
+    
     //画面遷移
-    @State var page : Int? = 0
+    //@State var page : Int? = 0
     @State var isShowSecondView: Bool = false
+    @State var timeMoreZero:Bool = false
     
     var body: some View {
         NavigationView {
             VStack() {
                 HStack(spacing : 0){
-                    TimePicker(num: $hour, maxnum : 24,minnum : 0).frame(width: 70)
+                    pickerView(num: $hour, maxnum : 24,minnum : 0,hour: $hour,minute: $minute,seconds: $seconds,timeMoreZero: $timeMoreZero)
                     Text("時間").frame(width: 50, height: 50, alignment: .leading)
-                    TimePicker(num: $minute, maxnum : 60, minnum: 0).frame(width: 70)
+                    pickerView(num: $minute, maxnum : 60,minnum : 0,hour: $hour,minute: $minute,seconds: $seconds,timeMoreZero: $timeMoreZero)
                     Text("分").frame(width:32,height: 50, alignment: .leading)
-                    TimePicker(num: $seconds, maxnum : 60, minnum :5).frame(width: 70)
+                    pickerView(num: $seconds, maxnum : 60, minnum :0,hour: $hour,minute: $minute,seconds: $seconds,timeMoreZero: $timeMoreZero)
                     Text("秒").frame(width:32,height: 50, alignment: .leading)
                 }
                 HStack(spacing:20){
@@ -42,12 +43,16 @@ struct ContentView: View {
                         }
                     }) {
                         VStack(){
-                            Text("セット数").padding(.horizontal).padding(.top)
-                            Text("\(self.setNum) 回").padding()
-                            Spacer()
+                            Text("セット数").padding(.horizontal)
+                            Text("\(self.setNum) 回").font(.title3)
                         }
-                        .frame(width: 150.0, height: 80.0)
-                        .foregroundColor(.white).background(Color.blue).cornerRadius(10).padding(.leading)
+                        .frame(width: 150.0, height: 60.0)
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .clipShape(Capsule())
+                        //.cornerRadius(10)
+                        .padding(.leading)
+                        .shadow(radius: 5)
                     }
                     //オプションの選択を可能にする
                     Button(action: {
@@ -57,44 +62,52 @@ struct ContentView: View {
                         }
                     }) {
                         VStack(){
-                            Text("休憩").padding(.horizontal).padding(.top)
-                            Text("\(self.restSecond) 秒").padding()
-                            Spacer()
+                            Text("休憩").padding(.horizontal)
+                            Text("\(self.restSecond) 秒").font(.title3)
                         }
-                        .frame(width: 150.0, height: 80.0)
+                        .frame(width: 150.0, height: 60.0)
                         .foregroundColor(.white)
-                        .background(Color.green).cornerRadius(10).padding(.trailing)
-                    }
+                        .background(Color.blue)
+                        .clipShape(Capsule())
+                        //.cornerRadius(10)
+                        .padding(.trailing)
+                        .shadow(radius: 5)                    }
                 }
                 ZStack{
-                    Button(action: {
-                        page = 1
-                    }) {
-                        Text("開始")
-                            .padding()
-                    }.frame(width: 320.0, height: 60.0)
-                        .foregroundColor(.white)
-                        .background(Color.orange).cornerRadius(10).padding(.horizontal)
-
                     NavigationLink(destination:
-                                    //SecondView(totaltime:CGFloat((self.hour*3600)+(self.minute*60)+self.seconds),
-                                   SecondView(totaltime:calcTime(hour: self.hour,minute: self.minute,seconds: self.seconds),
-                                               setNum:self.setNum,
-                                               restSecond:self.restSecond),
-                                   tag: 1, selection: $page) {  }
+                                    SecondView(totaltime:calcTime(hour: self.hour,minute: self.minute,seconds: self.seconds),setNum:self.setNum,restSecond:self.restSecond,isShowSecondView: $isShowSecondView).navigationBarHidden(true),isActive: $isShowSecondView){}
+                    
+                    Button(action: {
+                        self.isShowSecondView = true
+                    }) {
+                        HStack(spacing:15){
+                            Image(systemName: "play.fill").foregroundColor(.white)
+                            
+                            Text("開始").font(.title3)
+                                .foregroundColor(.white)
+                            //.background(Color.orange).cornerRadius(10).padding(.horizontal)
+                        }
+                        .disabled(!timeMoreZero)
+                            .frame(width: 320.0,height: 60.0)
+                            .background(Color.red)
+                            .clipShape(Capsule())
+                            .shadow(radius: 5)
+                    }
+                    
+                    //totaltimeが0の場合はボタンを非活性に見せる
+                    Button(action: {
+                    }) {}
+                        .frame(width: 320.0, height: 60.0)
+                        .background(self.timeMoreZero ? Color(red: 0.78, green: 0.78, blue: 0.78, opacity: 0.0) :Color(red: 0.78, green: 0.78, blue: 0.78, opacity: 0.5))
+                        .clipShape(Capsule())
                     
                     //下に表示するPickerのView
-                    underPicker(setNum: self.$setNum, showPicker: self.$showPickerSet, minNum: 1,maxNum: 50,unit: "回").offset(y: self.showPickerSet ? 0 : UIScreen.main.bounds.height).frame(alignment: .bottom)
+                    underPicker(setNum: self.$setNum, showPicker: self.$showPickerSet, minNum: 1,maxNum: 51,unit: "回").offset(y: self.showPickerSet ? 0 : UIScreen.main.bounds.height).frame(alignment: .bottom)
                     
-                    underPicker(setNum: self.$restSecond, showPicker: self.$showPickerRest, minNum: 5,maxNum: 1000,unit: "秒").offset(y: self.showPickerRest ? 0 : UIScreen.main.bounds.height).frame(alignment: .bottom)
+                    underPicker(setNum: self.$restSecond, showPicker: self.$showPickerRest, minNum: 5,maxNum: 1001,unit: "秒").offset(y: self.showPickerRest ? 0 : UIScreen.main.bounds.height).frame(alignment: .bottom)
                 }
             }
-        }
-    }
-    func calcTime(hour:Int,minute:Int,seconds:Int) -> CGFloat {
-        //secondsの最小値の補正のため+5する
-        let totaltime = hour * 3600 + minute * 60 + (seconds + 5)
-        return CGFloat(totaltime)
+        }.navigationBarHidden(true)
     }
 }
 
@@ -140,3 +153,34 @@ struct underPicker: View {
     }
 }
 
+struct pickerView: View{
+    @Binding var num:Int
+    var maxnum:Int
+    var minnum:Int
+    
+    @Binding var hour: Int
+    @Binding var minute: Int
+    @Binding var seconds: Int
+    
+    @Binding var timeMoreZero :Bool
+    
+    var body: some View {
+        TimePicker(num: $num, maxnum : maxnum,minnum : minnum)
+            .onChange(of: num){ value in
+                if checkTime(hour: self.hour,minute: self.minute,seconds: self.seconds){
+                    timeMoreZero = true
+                }else{
+                    timeMoreZero = false
+                }
+            }.frame(width: 70)
+    }
+}
+
+func calcTime(hour:Int,minute:Int,seconds:Int) -> CGFloat {
+    let totaltime = hour * 3600 + minute * 60 + seconds
+    return CGFloat(totaltime)
+}
+
+func checkTime(hour:Int,minute:Int,seconds:Int) -> Bool{
+    calcTime(hour:hour,minute:minute,seconds:seconds) > 0
+}
--- Timer/SecondView.swift
+++ Timer/SecondView.swift
@@ -12,37 +12,63 @@ struct SecondView: View {
     var totaltime : CGFloat
     var setNum : Int
     var restSecond : Int
+    @Binding var isShowSecondView : Bool
     
     @State var outputSeconds : CGFloat = 5
     
-    @State var start : Bool = true
-    @State var reststart : Bool = true
+    @State var isFinished :Bool = false
+    @State var isStop :Bool = false
+    @State var start : Bool = false
+    
+    @State var setCount : Int = 0
     
     @State var to : CGFloat = 0
     @State var count : CGFloat = 0
     @State var time = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
+    @State var page:Int? = 0
+    
     var body: some View {
-        
         VStack{
             //Text("\(self.totaltime)")
-            CircleV(to : self.$to,count : self.$count,totaltime : self.totaltime)
-            ButtonV(start : self.$start,to : self.$to,count : self.$count,totaltime : self.totaltime)
-        }
-        //タイマー関連の制御
-        .onReceive(self.time) { (_) in
-            if self.reststart{
-                
-            }
-            if self.start{
-                if self.count <= self.totaltime{
-                    self.count += 0.1
-                    //進捗バーをアニメーションで変化させる
-                    withAnimation(.default){
-                        self.to = CGFloat(self.count) / CGFloat(self.totaltime)
+            CircleV(to : self.$to,count : self.$count,start: self.start,outputSeconds : self.outputSeconds)
+            optionInfo(setNum: self.setNum,setCount: self.setCount,restSecond: self.restSecond,totaltime : self.totaltime)
+            ButtonV1(isStop : self.$isStop,to : self.$to,count : self.$count,totaltime : self.totaltime,
+                     setCount : self.$setCount,
+                     setNum : self.setNum,outputSeconds: self.$outputSeconds,
+                     isShowSecondView : $isShowSecondView,isFinished : self.$isFinished,start : self.$start)
+            
+            //タイマー関連の制御
+            .onReceive(self.time) { (_) in
+                if !isStop{
+                    //継続中
+                    if self.count <= self.outputSeconds{
+                        self.count += 0.1
+                        //進捗バーをアニメーションで変化させる
+                        withAnimation(.default){
+                            self.to = CGFloat(self.count) / CGFloat(self.outputSeconds)
+                        }
+                    //終了
+                    }else{
+                        //start=trueの場合は、終了条件を確認する
+                        if start {
+                            self.setCount += 1
+                            if self.setCount == self.setNum{
+                                self.isStop.toggle()
+                                self.isFinished.toggle()
+                            }
+                        }
+                        
+                        if self.setCount != self.setNum{
+                            self.outputSeconds = self.totaltime
+                            self.count = 0
+                        }
+                        
+                        self.start.toggle()
+                        
+                        //進捗バーをリセット※アニメーションなし
+                        self.to = CGFloat(self.count) / CGFloat(self.outputSeconds)
                     }
-                }else{
-                    self.start.toggle()
                 }
             }
         }
@@ -52,7 +78,8 @@ struct SecondView: View {
 struct CircleV : View{
     @Binding var to: CGFloat
     @Binding var count: CGFloat
-    var totaltime:CGFloat
+    var start: Bool
+    var outputSeconds:CGFloat
     
     var body: some View{
         ZStack{
@@ -60,92 +87,123 @@ struct CircleV : View{
             //storoke 円を輪にする
             //linecap 円の形
             //trim トリミング
-                .stroke(Color.black.opacity(0.09), style: StrokeStyle(lineWidth: 35,lineCap: .round))
+                .stroke(Color.black.opacity(0.09), style: StrokeStyle(lineWidth: 15,lineCap: .round))
                 .frame(width: 280, height: 280)
             
             //rotationEffect 起点を指定して、回転させる
             Circle().trim(from: (1 - self.to), to: 1)
-                .stroke(Color.red, style: StrokeStyle(lineWidth: 35,lineCap: .round))
+                .stroke(Color( self.start  ? .red : .blue ), style: StrokeStyle(lineWidth: 15,lineCap: .round))
                 .frame(width: 280, height: 280)
                 .rotationEffect(.init(degrees: -90))
             
             VStack{
-                Text(printdata(totaltimemin:TimeInterval(self.totaltime - self.count))).font(.system(size: 65))
+                Text(printdata(totaltimemin:TimeInterval(self.outputSeconds - self.count + 0.99999999))).font(.system(size: 65))
                     .fontWeight(.bold)
-                
-                Text(printdata(totaltimemin:TimeInterval(self.count))).font(.title)
-                Text(printdata(totaltimemin:TimeInterval(self.totaltime))).font(.title)
             }
         }
     }
-    func printdata(totaltimemin:TimeInterval) -> String{
-        let dateFormatter = DateComponentsFormatter()
-        
-        dateFormatter.unitsStyle = .positional
-        dateFormatter.allowedUnits = [.hour, .minute, .second]
-        return (dateFormatter.string(from: totaltimemin)!)
-    }
 }
 
-struct ButtonV : View{
-    @Binding var start : Bool
+struct ButtonV1 : View{
+    @Binding var isStop : Bool
     @Binding var to : CGFloat
     @Binding var count : CGFloat
     var totaltime: CGFloat
+    @Binding var setCount : Int
+    var setNum : Int
+    
+    @Binding var outputSeconds : CGFloat
+    @Binding var isShowSecondView : Bool
+    @Binding var isFinished : Bool
+    @Binding var start : Bool
     
     var body: some View{
         //spacing 余白を設定する
         HStack(spacing:20){
             Button(action: {
-                if self.count >= self.totaltime{
+                self.isStop.toggle()
+                if (isFinished) {
+                    
+                    self.outputSeconds = 5
+                    
+                    self.isFinished = false
+                    self.start = false
+                    self.isStop = false
+                    
+                    self.setCount = 0
+                    self.to = 0
                     self.count = 0
-                    withAnimation(.default){
-                        self.to = 0
-                    }
                 }
-                
-                self.start.toggle()
-                
             }){
                 HStack(spacing:15){
-                    Image(systemName: self.start ? "pause.fill" : "play.fill")
+                    Image(systemName: self.isStop ? "play.fill" : "pause.fill")
                         .foregroundColor(.white)
                     
-                    Text(self.start ? "pause":"play")
+                    Text(self.isStop ? "再開":"一時停止")
                         .foregroundColor(.white)
                 }
                 .padding(.vertical)
-                .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+                .frame(width: 150.0)
                 .background(Color.red)
                 .clipShape(Capsule())
             }
             
             Button(action: {
-                
-                self.count = 0
-                
-                withAnimation(.default){
-                    self.to = 0
-                }
+                isShowSecondView = false
             }){
                 HStack(spacing:15){
                     Image(systemName: "arrow.clockwise")
                         .foregroundColor(.red)
                     
-                    Text("Restart")
+                    Text("終了")
                         .foregroundColor(.red)
                 }
                 .padding(.vertical)
-                .frame(width: (UIScreen.main.bounds.width / 2) - 55)
+                .frame(width:  150.0)
                 .background(
-                    
                     Capsule()
-                        .stroke(Color.red, lineWidth: 2)
+                        .stroke(Color.red)//, lineWidth: 2)
                 )
             }
         }
-        .padding(.top, 55)
     }
 }
 
+struct optionInfo : View{
+    var setNum :Int
+    var setCount :Int
+    var restSecond :Int
+    var totaltime:CGFloat
+    
+    var body:some View{
+        HStack(spacing:20){
+            
+            RoundedRectangle(cornerRadius:10)
+                .foregroundColor(Color.green).frame(width: 150.0, height: 60.0).padding(.leading)
+                .overlay(
+                    VStack(){
+                        Text("セット数").padding(.horizontal).padding(.leading)
+                        Text("\(self.setCount) / \(self.setNum) 回").padding(.leading).font(.title3)
+                    }.foregroundColor(.white)
+                )
+            
+            RoundedRectangle(cornerRadius:10)
+                .foregroundColor(Color.blue).frame(width: 150.0, height: 60.0).padding(.trailing)
+                .overlay(
+                    VStack(){
+                        Text("休憩").padding(.horizontal).padding(.trailing)
+                        Text("\(self.restSecond) 秒").padding(.trailing).font(.title3)
+                    }.foregroundColor(.white)
+                )
+        }.padding(.top)
+    }
+}
 
+func printdata(totaltimemin:TimeInterval) -> String{
+    let dateFormatter = DateComponentsFormatter()
+    
+    dateFormatter.unitsStyle = .positional
+    dateFormatter.allowedUnits = [.hour, .minute, .second]
+    dateFormatter.zeroFormattingBehavior = .pad
+    return (dateFormatter.string(from: totaltimemin)!)
+}
